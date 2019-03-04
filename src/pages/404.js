c2ad02be7d5bd4f6abfdab81e224c5d7434b402f
hendhyhutomo <hendhyhutomo@gmail.com>
2019-03-04 15:57:22 +0700

    addded error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,14 @@
-import React from "react"
-import Layout from "components/layout"
+import React from 'react';
+import Layout from 'components/layout';
+import { Link } from 'gatsby';
 
-export default ({location, data}) => (
-    <Layout headerText="404" location={location}>
-        <div>
-            <h1>Pages List</h1>
-        </div>
-    </Layout>
-)
+export default () => (
+	<Layout className="error">
+		<Link to="/">
+			<div>
+				<div>Gema Semesta</div>404 Error
+			</div>
+			<footer>&copy; 2019. Gema Semesta</footer>
+		</Link>
+	</Layout>
+);
