db9983fd3430bc75739211fcdde6dcc4835dabc9
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2023-11-20 10:38:38 +0700

    💄 add tomostudio head credit

--- src/html.js
+++ src/html.js
@@ -1,14 +1,32 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React from "react";
+import PropTypes from "prop-types";
 
 export default class HTML extends React.Component {
   render() {
     return (
       <html {...this.props.htmlAttributes}>
         <head>
+          <head
+            dangerouslySetInnerHTML={{
+              __html: `${"<!--                                                -->"}
+            ${"<!-- 88888888   8888888   888    888   8888888          -->"}
+            ${"<!--    88    88888888888 8888  8888 88888888888        -->"}
+            ${"<!--    88    88888888888 88 8888 88 88888888888        -->"}
+            ${"<!--    88    88888888888 88  88  88 88888888888        -->"}
+            ${"<!--    88      8888888   88      88   8888888          -->"}
+            ${"<!-- 8888888 88888888 88     88 8888888  88   8888888   -->"}
+            ${"<!-- 88         88    88     88 88    88 88 88888888888 -->"}
+            ${"<!-- 8888888    88    88     88 88    88 88 88888888888 -->"}
+            ${"<!--      88    88    88     88 88    88 88 88888888888 -->"}
+            ${"<!-- 8888888    88     8888888  8888888  88   8888888   -->"}
+            ${"<!--                                                    -->"}
+            ${"<!-- site by https://tomostudio.id                      -->"}
+            ${"<!--                                                    -->"}`,
+            }}
+          />
           <meta charSet="utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-					<meta name="viewport" content="initial-scale=1, viewport-fit=cover" />
+          <meta name="viewport" content="initial-scale=1, viewport-fit=cover" />
           {this.props.headComponents}
         </head>
         <body {...this.props.bodyAttributes} className="loading">
@@ -21,7 +39,7 @@ export default class HTML extends React.Component {
           {this.props.postBodyComponents}
         </body>
       </html>
-    )
+    );
   }
 }
 
@@ -32,4 +50,4 @@ HTML.propTypes = {
   preBodyComponents: PropTypes.array,
   body: PropTypes.string,
   postBodyComponents: PropTypes.array,
-}
+};
